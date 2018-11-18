da1dbd0ba4c3c72a0b80a73259fdf5653fef3ca9
valkyrienyanko <sebastianbelle074@gmail.com>
2018-11-18 16:07:50 -0600

    commit

---
+++ CampfireBuffs.cs
@@ -0,0 +1,11 @@
+using Terraria.ModLoader;
+
+namespace CampfireBuffs
+{
+	class CampfireBuffs : Mod
+	{
+		public CampfireBuffs()
+		{
+		}
+	}
+}
---
+++ CampfireTile.cs
@@ -0,0 +1,25 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CampfireBuffs
+{
+    class CampfireTile : GlobalTile
+    {
+        public override void NearbyEffects(int i, int j, int type, bool closer) {
+            Player p = Main.LocalPlayer;
+            if (type == TileID.Campfire) {
+                if (closer)
+                {
+                    if (Main.tile[i, j].frameY < 36)// Is the campfire lit?
+                    {
+                        p.AddBuff(BuffID.Regeneration, 10);
+                        p.AddBuff(BuffID.ManaRegeneration, 10);
+                        p.AddBuff(BuffID.Heartreach, 10);
+                        p.AddBuff(BuffID.Shine, 10);
+                    }
+                }
+            }
+        }
+    }
+}
