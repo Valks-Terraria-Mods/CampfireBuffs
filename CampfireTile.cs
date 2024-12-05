505d2ef8743b352221fa64d1dbec8da74f4e7ed0
valkyrienyanko <sebastianbelle074@protonmail.com>
2024-12-04 19:42:58 -0600

    2.1

--- CampfireTile.cs
+++ CampfireTile.cs
@@ -2,17 +2,35 @@
 
 public class CampfireTile : GlobalTile
 {
-    public override void NearbyEffects(int i, int j, int type, bool closer) {
+    public override void NearbyEffects(int i, int j, int type, bool closer) 
+    {
         Player p = Main.LocalPlayer;
-        if (type == TileID.Campfire) {
+
+        if (type == TileID.Campfire) 
+        {
             if (closer)
             {
-                if (Main.tile[i, j].TileFrameY < 36)// Is the campfire lit?
+                if (Main.tile[i, j].TileFrameY < 36) // Is the campfire lit?
                 {
-                    p.AddBuff(BuffID.Regeneration, 10);
-                    p.AddBuff(BuffID.ManaRegeneration, 10);
-                    p.AddBuff(BuffID.Heartreach, 10);
-                    p.AddBuff(BuffID.Shine, 10);
+                    if (Config.Instance.HealthRegeneration)
+                    {
+                        p.AddBuff(BuffID.Regeneration, 10);
+                    }
+
+                    if (Config.Instance.ManaRegeneration)
+                    {
+                        p.AddBuff(BuffID.ManaRegeneration, 10);
+                    }
+
+                    if (Config.Instance.Heartreach)
+                    {
+                        p.AddBuff(BuffID.Heartreach, 10);
+                    }
+                    
+                    if (Config.Instance.Shine)
+                    {
+                        p.AddBuff(BuffID.Shine, 10);
+                    }
                 }
             }
         }
---
+++ Config.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace CampfireBuffs;
+
+public class Config : ModConfig
+{
+    [DefaultValue(true)]
+    public bool ManaRegeneration;
+
+    [DefaultValue(true)]
+    public bool HealthRegeneration;
+
+    [DefaultValue(true)]
+    public bool Heartreach;
+
+    [DefaultValue(true)]
+    public bool Shine;
+
+    public static Config Instance { get; private set; }
+
+    public override ConfigScope Mode => ConfigScope.ServerSide;
+
+    public override void OnLoaded()
+    {
+        Instance = this;
+    }
+}
